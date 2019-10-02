877d4c90212f38ea7c7084a166b6d8776581aed6
Junior Dugue <jrdugue@gmail.com>
2019-10-02 19:59:55 -0400

    added content to my components so they can be visible on screen, was also able to console log the data successfully

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -4,7 +4,7 @@ export default class Todo extends Component {
   render() {
     return (
       <div>
-        
+        <h2>Todo</h2>
       </div>
     )
   }
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -4,7 +4,7 @@ export default class TodoForm extends Component {
   render() {
     return (
       <div>
-        
+        <h2>Todo Form</h2>
       </div>
     )
   }
