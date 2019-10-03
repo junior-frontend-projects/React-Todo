59f755434c35c64cc73f93d23dd39e7ccfd681db
Junior Dugue <jrdugue@gmail.com>
2019-10-02 20:58:29 -0400

    added Date.now(), created handler functions, destrutured those handler functions, created form inside form component, added my submit handler, passed state via todo form and todo list components, passed task as a prop, added change event handler to input

--- src/App.js
+++ src/App.js
@@ -7,15 +7,46 @@ class App extends React.Component {
   // this component is going to take care of state, and any change handlers you need to work with your state
 
   state={
-    data: [{id: 1528817077286, task: "Organize Garage", completed: false,}, {id: 1528817084358, task: "Bake Cookies", completed: false, }]
+    data: [{id: 1528817077286, task: "Organize Garage", completed: false,}, {id: 1528817084358, task: "Bake Cookies", completed: false,}, {id: Date.now(), task: "", completed: false,}]
   }
+
+  handleChange = e => {
+    console.log("handle changes")
+  };
+
+  handleSubmit = e => {
+    console.log("handle submits")
+  };
+
+  clearCompleted = () => {
+    console.log("clears completed")
+  }
+
+  handleDelete = id => {
+    console.log(`"handles deletions of ${id}"`)
+  }
+
+  handleEdit = id => {
+    console.log(`edits ${id}`)
+  }
+
   render() {
     console.log(this.state);
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
-        <TodoList/>
-        <TodoForm/>
+        <TodoForm
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+          task={this.state.task}
+          completed={this.state.completed}
+        />
+        <TodoList
+          task={this.state.task}
+          clearCompleted={this.state.clearCompleted}
+          handleDelete={this.state.handleDelete}
+          handleEdit={this.state.handleEdit}
+        />
       </div>
     );
   }
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react'
 
 export default class TodoForm extends Component {
   render() {
+    const {task, handleChange, handleSubmit, completed} = this.props;
+
     return (
       <div>
         <h2>Todo Form</h2>
+        <form
+          onSubmit={handleSubmit}
+        >
+          <input
+            type="text"
+            placeholder="Add tasks here"
+            value={task}
+            onChange={handleChange}
+          />
+          <button type="submit">Add</button>
+        </form>
       </div>
     )
   }
